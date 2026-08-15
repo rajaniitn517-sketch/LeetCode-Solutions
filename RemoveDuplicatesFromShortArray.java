4fb6d092b1cd1d3e486606d5b2a21c0c47077360
Rajan kumar <rajaniitn517@gmai.com>
2026-08-15 19:24:35 +0530

    Add RemoveDuplicatesFromShortArray.java

---
+++ RemoveDuplicatesFromShortArray.java
@@ -0,0 +1,15 @@
+class Solution{
+public class RemoveDuplicatesFromShortArray {
+     if(nums.length ==0){// jb arra khali ho tb 
+            return 0;
+        }
+            int i = 0;
+            for(int j =1; j<nums.length; j++){
+                if(nums[j] != nums[i]){
+                    i++;
+                    nums[i]= nums[j];
+                }
+            }
+            return i+1;
+        }
+}
